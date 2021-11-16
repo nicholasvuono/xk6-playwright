5da411f5085e2a836f4cb92510555c4ecfc85b83
nicholasvuono <nicholas.vuono@gmail.com>
2021-11-15 19:37:05 -0500

    chore: add generic test, add go report card

---
+++ playwright_test.go
@@ -0,0 +1,30 @@
+package playwright
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/mxschmitt/playwright-go"
+)
+
+var tests = []func(t *testing.T){
+	TestPlaywright,
+}
+
+func TestPlaywright(t *testing.T) {
+	var pw Playwright
+	var opts [] string
+	var opts2 playwright.PageGotoOptions
+	var opts3 playwright.PageWaitForSelectorOptions
+
+	pw.Launch(opts)
+	pw.NewPage()
+	pw.Goto("https://www.google.com", opts2)
+	pw.WaitForSelector("//html/body/div[1]/div[2]", opts3)
+}
+
+func TestAll(t *testing.T) {
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), test)
+	}
+}
