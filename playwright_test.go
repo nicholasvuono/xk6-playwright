f0feec3ad2581d2288cbfe6b687bbb2c965514e8
egor-romanov <egor.romanov@gmail.com>
2023-02-03 17:24:12 +0300

    add func to launch browser with persistent context

--- playwright_test.go
+++ playwright_test.go
@@ -2,6 +2,7 @@ package playwright
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 	"testing"
 
@@ -12,6 +13,7 @@ var tests = []func(t *testing.T){
 	TestPlaywright,
 	TestPlaywright2,
 	TestCookies,
+	TestPersistentContext,
 }
 
 func TestPlaywright(t *testing.T) {
@@ -25,8 +27,8 @@ func TestPlaywright(t *testing.T) {
 
 	pw.Launch(opts)
 	pw.NewPage()
-	pw.Goto("https://www.google.com", opts2)
-	pw.WaitForSelector("input[title='Search']", opts3)
+	pw.Goto("https://www.github.com", opts2)
+	pw.WaitForSelector("input[name='q']", opts3)
 	pw.Kill()
 }
 
@@ -42,9 +44,9 @@ func TestPlaywright2(t *testing.T) {
 
 	pw.Launch(opts)
 	pw.NewPage()
-	pw.Goto("https://www.google.com", opts2)
-	pw.WaitForSelector("input[title='Search']", opts3)
-	pw.Type("input[title='Search']", "how to measure real user metrics with the xk6-playwright extension for k6?", opts4)
+	pw.Goto("https://www.github.com", opts2)
+	pw.WaitForSelector("input[name='q']", opts3)
+	pw.Type("input[name='q']", "how to measure real user metrics with the xk6-playwright extension for k6?", opts4)
 	fp := pw.FirstPaint()
 	fcp := pw.FirstContentfulPaint()
 	ttmi := pw.TimeToMinimallyInteractive()
@@ -63,6 +65,22 @@ func TestPlaywright3(t *testing.T) {
 
 	pw.Connect("http://localhost:9222", opts)
 	pw.Goto("https://www.unqork.com", opts2)
+}
+
+func TestPersistentContext(t *testing.T) {
+	var pw Playwright
+	headless := true
+	opts := playwright.BrowserTypeLaunchPersistentContextOptions{
+		Headless: &headless,
+	}
+	var opts2 playwright.PageGotoOptions
+	var opts3 playwright.PageWaitForSelectorOptions
+
+	pw.LaunchPersistent("./tmp/context-"+strconv.Itoa(rand.Intn(1000)), opts)
+	pw.NewPage()
+	pw.Goto("https://www.github.com", opts2)
+	pw.WaitForSelector("input[name='q']", opts3)
+	pw.Kill()
 }
 
 func TestCookies(t *testing.T) {
