4ae48d2f5a58a0943619af25a5f694e9a9c6dc96
Nicholas Vuono <nick@accessos.io>
2024-02-22 23:11:08 -0500

    chore: minor updates to readme

--- examples/headless-false-example.js
+++ examples/headless-false-example.js
@@ -1,7 +1,7 @@
 import pw from 'k6/x/playwright';
 
 export default function () {
-  pw.launch({headless: false,})
+  pw.launch({headless: false})
   pw.newPage()
   pw.goto("https://www.github.com/", { waitUntil: 'networkidle'})
   pw.waitForSelector(".search-input", {state: 'visible'})
