9892846d9f8ca4ce09005276a81c9c6359adb408
Nicholas Vuono <nick@accessos.io>
2024-02-22 22:42:51 -0500

    chore: add headless false test example

---
+++ examples/headless-false-example.js
@@ -0,0 +1,9 @@
+import pw from 'k6/x/playwright';
+
+export default function () {
+  pw.launch({headless: false,})
+  pw.newPage()
+  pw.goto("https://www.github.com/", { waitUntil: 'networkidle'})
+  pw.waitForSelector(".search-input", {state: 'visible'})
+  pw.kill()
+}
